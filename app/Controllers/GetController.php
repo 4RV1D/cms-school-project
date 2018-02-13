5d5ae0fe0d295ef50cc856dfc56545a9c2eca0bf
Arvid Gullqvist <arvidgullqvist@hotmail.com>
2018-02-13 04:24:07 +0100

    Slim3 framework added
    added the slim3 framework and some pre scaffolding

---
+++ app/Controllers/GetController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Cms;
+
+
+class GetController extends Controller
+{
+
+  public function test($request, $response) {
+
+    echo "Heklo gus!";
+
+  }
+
+}
